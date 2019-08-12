1ce2cafcd67fb1f923a0e9ee2d0d08f07f660d25
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-12 18:53:20 -0400

    embedded `about-section.php` component in `front-page.php`

--- wp-content/themes/bootstrap.single-page/footer.php
+++ wp-content/themes/bootstrap.single-page/footer.php
@@ -1,4 +1,19 @@
 <?php wp_footer(); ?>
-...but did it even work bro?
+
+
+
+
+  <!-- ------------------------------------------------------------------------ -->
+  <!-- Footer contains content at the bottom of the page -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; Your Website 2019
+    </div>
+  </footer>
+  <!-- Footer ends here -->
+  <!-- ------------------------------------------------------------------------ -->
+
+
+
 </body>
 </html>
--- wp-content/themes/bootstrap.single-page/front-page.php
+++ wp-content/themes/bootstrap.single-page/front-page.php
@@ -10,18 +10,7 @@
 
 
   <!-- About Section begins here -->
-  <section id="about" class="about-section text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 mx-auto">
-          <h2 class="text-white mb-4">Built with Bootstrap 4</h2>
-          <p class="text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-      <img src="<?php bloginfo('template_directory'); ?>/resources/img/ipad.png">
-    </div>
-  </section>
+  <?php include 'about-section.php';?>
   <!-- About Section ends here -->
 
 
@@ -196,19 +185,6 @@
 
 
 
-
-
-
-
-  <!-- ------------------------------------------------------------------------ -->
-  <!-- Footer contains content at the bottom of the page -->
-  <footer class="bg-black small text-center text-white-50">
-    <div class="container">
-      Copyright &copy; Your Website 2019
-    </div>
-  </footer>
-  <!-- Footer ends here -->
-  <!-- ------------------------------------------------------------------------ -->
 
 
 
