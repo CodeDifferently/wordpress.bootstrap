8550d583e6ae77ecb56f3b1d4688bc4cabf858d1
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-13 18:29:17 -0400

    asset injection working

---
+++ wp-content/themes/bootstrap.online-store/components/purchase-page/body.php
@@ -0,0 +1,28 @@
+<div class="col-lg-3">
+    <h1 class="my-4">Shop Name</h1>
+    <div class="list-group">
+        <a href="#" class="list-group-item active">Category 1</a>
+        <a href="#" class="list-group-item">Category 2</a>
+        <a href="#" class="list-group-item">Category 3</a>
+    </div>
+</div>
+<!-- /.col-lg-3 -->
+
+<div class="col-lg-9">
+    <div class="card mt-4">
+        <img class="card-img-top img-fluid" src="<?php bloginfo('template_directory'); ?>/resources/img/placeholder-900x400.png">
+        <!-- please insert this image -->
+        <?php insertImage("/resources/img/placeholder-900x400.png") ?>
+        <!-- please insert this image -->
+        <div class="card-body">
+            <h3 class="card-title">Product Name</h3>
+            <h4>$24.99</h4>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores,
+                sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!
+            </p>
+            <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
+            4.0 stars
+        </div>
+    </div>
