9eba4e18a00c77514cd20e1cdcf9b51ffb67d1db
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-14 18:06:03 -0400

    added utility php functions

--- wp-content/themes/bootstrap.online-store/components/front-page/body.php
+++ wp-content/themes/bootstrap.online-store/components/front-page/body.php
@@ -3,8 +3,8 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
-        <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-        <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
+        <h1 class="mt-5">CodeDifferently Wordpress Starter Template</h1>
+        <p class="lead">Complete with bootstrap-4, jquery-3, utility php functions, and responsive navigation!</p>
         <ul class="list-unstyled">
           <li>View the <i>Shopping</i> page by clicking <?php insertLink("/wordpress/shop-page") ?>here</a>.</li>
           <li>View the <i>Purchase</i> page by clicking <?php insertLink("/wordpress/purchase-page") ?>here</a>.</li>
--- wp-content/themes/bootstrap.online-store/resources/libs/asset-injector.php
+++ wp-content/themes/bootstrap.online-store/resources/libs/asset-injector.php
@@ -12,14 +12,19 @@
 
 <?php 
 function insertImage($path, $additional_attributes='') {
-    echo "<img src=\"" . get_template_directory_uri() . $path . "\" " . $additional_attributes .">";
+    echo getInsertImage($path, $additional_attributes);
 }
 
 function insertLink($path, $additional_attributes='') {
-    echo "<a href=\"" . get_template_directory_uri () . $path . "\" " . $additional_attributes .">";
+    echo getInsertLink($path, $additional_attributes);
 }
 
-function includeFileWithVariables($fileName, $variables) {
-    extract($variables);
-    include($fileName);
- }
+
+function getInsertLink($path, $additional_attributes='') {
+    return "<a href=\"" . get_template_directory_uri () . $path . "\" " . $additional_attributes .">";
+}
+
+
+function getInsertImage($path, $additional_attributes='') {
+    return "<img src=\"" . get_template_directory_uri() . $path . "\" " . $additional_attributes .">";
+}
